9956e3dac8e8857c88fa81efa1a91124309f002d
jonas-andrade <jonas6116m@outlook.com>
2021-06-16 15:18:15 -0300

    initial

---
+++ jarvis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "---------------------------------------------------------_"
+echo "------------           J.A.R.V.S        ------------------"
+echo ".........................................................."
+
+echo "1 --> J2dex"
+echo "2 --> ?"
+echo "3 --> ?"
+echo "4 --> ?"
+echo "5 --> ?"
+read  op
+
+  if [ $op -eq 1 ];then
+echo " "
+
+elif [ $op -eq 2 ];then
+echo " "
+elif [ $op -eq 3 ];then
+echo " "
+elif [ $op -eq 4 ];then
+echo " "
+elif [ $op -eq 5 ];then
+echo " "
+fi
